7c53953ec1bbee6b4474926820deda46fd736c9b
Himanshu22omnie <147649649+Himanshu22omnie@users.noreply.github.com>
2023-11-01 13:33:13 +0530

    Delete DatabaseAutharization directory

--- DatabaseAutharization/src/main/java/com/config/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.config;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class Demo {
-	public static void main(String[] args) {
-		System.out.println(new BCryptPasswordEncoder().encode("1234"));
-	}
-}
